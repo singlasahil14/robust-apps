4655339238c57cabb4746f68369923cfd82d296c
SAHIL SINGLA <singlasahil14@gmail.com>
2020-06-10 14:24:29 -0400

    First commit

---
+++ user_constants.py
@@ -0,0 +1,9 @@
+# Path to dataset 
+DATA_PATH_DICT = {
+    'CIFAR': '/tmp/',
+    'RestrictedImageNet': '/fs/cml-datasets/ImageNet/ILSVRC2012',
+    'ImageNet': '/fs/cml-datasets/ImageNet/ILSVRC2012',
+    'H2Z': '/scratch/datasets/A2B/horse2zebra',
+    'A2O': '/scratch/datasets/A2B/apple2orange',
+    'S2W': '/scratch/datasets/A2B/summer2winter_yosemite'
+}
